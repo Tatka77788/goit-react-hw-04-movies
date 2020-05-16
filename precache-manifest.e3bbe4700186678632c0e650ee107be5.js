2889296be6616c1e620195d618d114ea58dafbba
Tetiana <tatka777t@gmail.com>
2020-05-16 17:32:26 +0300

    Updates

---
+++ precache-manifest.e3bbe4700186678632c0e650ee107be5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79d935ade183d5d4b1b1d7f092161543",
+    "url": "/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "83078e59e2081c88071a",
+    "url": "/goit-react-hw-04-movies/static/css/home-page.1e9b270c.chunk.css"
+  },
+  {
+    "revision": "f24491dc1f143370d885",
+    "url": "/goit-react-hw-04-movies/static/css/main.bf7bd832.chunk.css"
+  },
+  {
+    "revision": "0bdbb5089d76030739ea",
+    "url": "/goit-react-hw-04-movies/static/css/movie-details-page.373f45e8.chunk.css"
+  },
+  {
+    "revision": "ab902a002fa359832c2e",
+    "url": "/goit-react-hw-04-movies/static/css/movies-page.71a7f149.chunk.css"
+  },
+  {
+    "revision": "7cfac785ce9d41229c19",
+    "url": "/goit-react-hw-04-movies/static/js/0.b6430813.chunk.js"
+  },
+  {
+    "revision": "4ef0787563e364eca8a3",
+    "url": "/goit-react-hw-04-movies/static/js/6.fb651c4e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-04-movies/static/js/6.fb651c4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83078e59e2081c88071a",
+    "url": "/goit-react-hw-04-movies/static/js/home-page.e97f02dc.chunk.js"
+  },
+  {
+    "revision": "f24491dc1f143370d885",
+    "url": "/goit-react-hw-04-movies/static/js/main.213b282b.chunk.js"
+  },
+  {
+    "revision": "0bdbb5089d76030739ea",
+    "url": "/goit-react-hw-04-movies/static/js/movie-details-page.ebe31b25.chunk.js"
+  },
+  {
+    "revision": "ab902a002fa359832c2e",
+    "url": "/goit-react-hw-04-movies/static/js/movies-page.8edaa2ac.chunk.js"
+  },
+  {
+    "revision": "1bf8c8ca150d3f6f50f5",
+    "url": "/goit-react-hw-04-movies/static/js/runtime-main.d5f8bd14.js"
+  }
+]);
